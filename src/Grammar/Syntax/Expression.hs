828910e8af26ff093212fb0e951030aa8266ca5a
Godalin <1198080313@qq.com>
2021-11-02 23:25:43 +0800

    add bool data type andbuild comparison expressions

--- src/Grammar/Syntax/Expression.hs
+++ src/Grammar/Syntax/Expression.hs
@@ -3,13 +3,27 @@
 
 
 module Grammar.Syntax.Expression
-    ( ptExpression
-    , Expression
-    ) where
+    -- ( ptExpression
+    -- , Expression
+    -- ) where
+        where
 import           Grammar.Lexical.Basic
 import           Grammar.Syntax.Basic
-import           Parser.Basic
 import           Parser.Conbinators
+
+
+{-
+* simple maths expressions
+* <Expression> ::= <Expression> ([+-] <Expression>)*
+* <Factor>     ::= <Term>       ([*/] <Term>)*
+* <Term>       ::= <Num> | <Var> | <Expression>
+-}
+
+class Operator2x a b where
+    build2Op :: Token -> (a -> a -> b)
+
+class Operator1x a b where
+    build1Op :: Token -> (a -> b)
 
 data Expression
     = ExpJst Factor
@@ -35,12 +49,12 @@ ptExpression = (ptFactor >>> formExp) <+> cIter (ptOp1 <+> (ptFactor >>> formExp
         >>> uncurry (foldl step) where
             formExp (FacJst (TermExp e)) = e
             formExp e                    = ExpJst e
-            step t1 (opToken, t2) = buildOp opToken t1 t2
+            step t1 (opToken, t2) = build2Op opToken t1 t2
 
 ptFactor :: PLex Factor
 ptFactor = (ptTerm >>> FacJst) <+> cIter (ptOp2 <+> (ptTerm >>> FacJst))
         >>> uncurry (foldl step) where
-            step t1 (opToken, t2) = buildOp opToken t1 t2
+            step t1 (opToken, t2) = build2Op opToken t1 t2
 
 ptTerm :: PLex Term
 ptTerm = ptIntConstant >>> (\(TIntConstant idName) -> TermNum idName)
@@ -48,17 +62,15 @@ ptTerm = ptIntConstant >>> (\(TIntConstant idName) -> TermNum idName)
      <|> cEncloseEx (ptSingleMark '(') (ptSingleMark ')') ptExpression
         >>> TermExp
 
-class BiOperator a where
-    buildOp :: Token -> (a -> a -> a)
 
-instance BiOperator Factor where
-    buildOp TMul = FacMul
-    buildOp TDiv = FacDiv
-    buildOp TMod = FacMod
+instance Operator2x Factor Factor where
+    build2Op TMul = FacMul
+    build2Op TDiv = FacDiv
+    build2Op TMod = FacMod
 
-instance BiOperator Expression where
-    buildOp TAdd = ExpAdd
-    buildOp TSub = ExpSub
+instance Operator2x Expression Expression where
+    build2Op TAdd = ExpAdd
+    build2Op TSub = ExpSub
 
 ptOp2 :: PLex Token
 ptOp2 = ptToken <=> (`elem` [TMul, TDiv, TMod])
@@ -79,6 +91,49 @@ instance Show Factor where
     show (FacMod f1 f2) = "<" ++ show f1 ++ "%" ++ show f2 ++ ">"
 
 instance Show Term where
-    show (TermNum intVal) = "<N:" ++ intVal ++ ">"
-    show (TermVar idName) = "<V:" ++ idName ++ ">"
+    show (TermNum intVal) = "<N:" ++ intVal   ++ ">"
+    show (TermVar idName) = "<V:" ++ idName   ++ ">"
     show (TermExp exp)    = "<E:" ++ show exp ++ ">" -- This will never be used
+
+
+
+data BExpression
+    = BJst Expression
+    | BNot BExpression
+    | BAnd BExpression BExpression
+    | BOr  BExpression BExpression
+    deriving Show
+
+data BComparison
+    = Expression `BCEQ` Expression
+    | Expression `BCNE` Expression
+    | Expression `BCGT` Expression
+    | Expression `BCLT` Expression
+    | Expression `BCGE` Expression
+    | Expression `BCLE` Expression
+    deriving Show
+
+instance Operator2x BExpression BExpression where
+    build2Op TAnd = BAnd
+    build2Op TOr  = BOr
+
+ptOpBool2 :: PLex Token
+ptOpBool2 = ptToken <=> (`elem` [TAnd, TOr])
+
+ptBNot :: PLex Token
+ptBNot = ptToken <=> (== TNot)
+
+instance Operator2x Expression BComparison where
+    build2Op TEq = BCEQ
+    build2Op TNe = BCNE
+    build2Op TGt = BCGT
+    build2Op TLt = BCLT
+    build2Op TGe = BCGE
+    build2Op TLe = BCLE
+
+ptOpComparison :: PLex Token
+ptOpComparison = ptToken <=> (`elem` [TEq, TNe, TGt, TLt, TGe, TLe])
+
+ptComparison :: PLex BComparison
+ptComparison = ptExpression <+> ptOpComparison <+> ptExpression
+                >>> (\((l, op), r) -> build2Op op l r)
